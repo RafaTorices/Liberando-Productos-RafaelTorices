cafa8c0580aea5996ff28b48164c7aba4da83193
RAFAEL TORICES <torices.rafael@gmail.com>
2023-12-21 10:14:10 +0100

    function mysql init

--- src/application/app.py
+++ src/application/app.py
@@ -2,10 +2,12 @@
 Module define fastapi server configuration
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from hypercorn.asyncio import serve
 from hypercorn.config import Config as HyperCornConfig
 from prometheus_client import Counter
+from pydantic import BaseModel
+import mysql.connector
 
 app = FastAPI()
 
@@ -22,6 +24,17 @@ CREATE_STUDENT_ENDPOINT_REQUESTS = Counter(
 # Counter for register the number calls in webserver to the get_students endpoint
 GET_STUDENTS_ENDPOINT_REQUESTS = Counter(
     'get_students_requests_total', 'Total number of requests to get_students endpoint')
+
+dbconfig = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="students"
+)
+
+
+class Item(BaseModel):
+    name: str
 
 
 class SimpleServer:
@@ -60,21 +73,33 @@ class SimpleServer:
 
     # Function for create_student endpoint to create new student in app
     @app.post("/create_student")
-    async def create_student(name: str):
+    async def create_student(item: Item):
         """Implement create_student endpoint"""
         # Increment counter used for register number calls to the endpoint create_student
         CREATE_STUDENT_ENDPOINT_REQUESTS.inc()
-        if name == "":
-            # Return error if the name is null
-            return {"msg": "Error, name is null"}
-        # Return the name of the student
-        return {"msg": f"Hello, {name}!!"}
+        try:
+            mycursor = dbconfig.cursor()
+            sql = "INSERT INTO students (name) VALUES (%s)"
+            values = (item.name,)
+            mycursor.execute(sql, values)
+            dbconfig.commit()
+            return {"msg": "OK!!"}
+        except Exception as e:
+            raise HTTPException(status_code=500, Detail=str(e))
 
     # Function for get_students endpoint to get all students in app
+
     @app.get("/get_students")
     async def get_students():
         """Implement get_students endpoint"""
         # Increment counter used for register the number calls in webserver to the endpoint get_students
         GET_STUDENTS_ENDPOINT_REQUESTS.inc()
         # Return the list of students
-        return {"msg": "Hello, students!!"}
+        try:
+            mycursor = dbconfig.cursor()
+            sql = "SELECT * FROM students"
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            return myresult
+        except Exception as e:
+            raise HTTPException(status_code=500, Detail=str(e))
