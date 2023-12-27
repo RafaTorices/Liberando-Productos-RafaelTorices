48746cc97132b82c1ba48ecb9d4e2ab97f100c6c
RAFAEL TORICES <torices.rafael@gmail.com>
2023-12-27 13:19:48 +0100

    sleep 30 in mysql create

--- tools/check_mysql_ready.sh
+++ tools/check_mysql_ready.sh
@@ -1,6 +1,7 @@
 while true; do
     if pgrep -x mysqld >/dev/null; then
         echo "MySQL is running."
+        sleep 30
         break
     else
         echo "MySQL is not running, waiting 10 seconds to retry..."
