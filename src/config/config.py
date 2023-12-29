d4fd6fb0ab7cad0d21f35326c91a06932295979d
RAFAEL TORICES <torices.rafael@gmail.com>
2023-12-29 21:55:54 +0100

    Config env database python app

--- src/config/config.py
+++ src/config/config.py
@@ -1,5 +1,13 @@
+import os
+
+# export DB_HOST="localhost"
+# export DB_USER="studentsuser"
+# export DB_PASSWORD="studentspassword"
+# export DB_DATABASE="students"
+
 DB_CONFIG = {
-    'user': 'studentsuser',
-    'password': 'studentspassword',
-    'database': 'students',
+    'host': os.environ["DB_HOST"],
+    'user': os.environ["DB_USER"],
+    'password': os.environ["DB_PASSWORD"],
+    'database': os.environ["DB_DATABASE"]
 }
